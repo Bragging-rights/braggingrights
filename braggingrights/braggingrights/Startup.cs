44eab4de636975765d9148ac1c093ff108bf9eb7
Gaffax <gaffax@gmail.com>
2016-01-19 23:24:01 +0100

    First commit. Setting up version control for cloud solution

---
+++ braggingrights/braggingrights/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(braggingrights.Startup))]
+namespace braggingrights
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
