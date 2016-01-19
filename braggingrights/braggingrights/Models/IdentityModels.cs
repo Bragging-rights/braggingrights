a7cb31c1ccc54961cf3634e3755ec6c0a1ed5a11
Gaffax <gaffax@gmail.com>
2016-01-19 23:44:28 +0100

    Add mobster model, controller and CRUD views

--- braggingrights/braggingrights/Models/IdentityModels.cs
+++ braggingrights/braggingrights/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace braggingrights.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<braggingrights.Models.Mobster> Mobsters { get; set; }
     }
 }
---
+++ braggingrights/braggingrights/Models/Mobster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace braggingrights.Models
+{
+    public class Mobster
+    {
+        public int mobsterId { get; set; }
+
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+    }
+}
